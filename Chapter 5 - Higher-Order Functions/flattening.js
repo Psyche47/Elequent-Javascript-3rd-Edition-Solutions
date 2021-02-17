e2aaa9cd2eee52fe9c238a264b186059e144c48b
Psyche47 <16-32498-2@student.aiub.edu>
2021-02-18 02:50:54 +0600

    Chapter 5 solutions updated.

--- Chapter 5 - Higher-Order Functions/flattening.js
+++ Chapter 5 - Higher-Order Functions/flattening.js
@@ -1,3 +1,8 @@
+/*
+Use the reduce method in combination with the concat method to “flatten”
+an array of arrays into a single array that has all the elements of the original
+arrays.
+*/
 let flattening = (array) => {
   return array.reduce((total, value) => {
     return total.concat(Array.isArray(value) ? flattening(value) : value);
